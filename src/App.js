faf956e11d6a712070571cb0d816364f818d5c1b
nafishasalsabil <nafishasalsabil16@gmail.com>
2023-11-04 22:42:18 +0600

    image made relative

--- src/App.js
+++ src/App.js
@@ -6,57 +6,57 @@ const App = () => {
   const [images, setImages] = useState([
     {
       id: 1,
-      imageUrl: "/images/image-1.webp",
+      imageUrl: "./images/image-1.webp",
       order: 1,
     },
     {
       id: 2,
-      imageUrl: "/images/image-2.webp",
+      imageUrl: "./images/image-2.webp",
       order: 2,
     },
     {
       id: 3,
-      imageUrl: "/images/image-3.webp",
+      imageUrl: "./images/image-3.webp",
       order: 3,
     },
     {
       id: 4,
-      imageUrl: "/images/image-4.webp",
+      imageUrl: "./images/image-4.webp",
       order: 4,
     },
     {
       id: 5,
-      imageUrl: "/images/image-5.webp",
+      imageUrl: "./images/image-5.webp",
       order: 5,
     },
     {
       id: 6,
-      imageUrl: "/images/image-6.webp",
+      imageUrl: "./images/image-6.webp",
       order: 6,
     },
     {
       id: 7,
-      imageUrl: "/images/image-7.webp",
+      imageUrl: "./images/image-7.webp",
       order: 7,
     },
     {
       id: 8,
-      imageUrl: "/images/image-8.webp",
+      imageUrl: "./images/image-8.webp",
       order: 8,
     },
     {
       id: 9,
-      imageUrl: "/images/image-9.webp",
+      imageUrl: "./images/image-9.webp",
       order: 9,
     },
     {
       id: 10,
-      imageUrl: "/images/image-10.jpeg",
+      imageUrl: "./images/image-10.jpeg",
       order: 10,
     },
     {
       id: 11,
-      imageUrl: "/images/image-11.jpeg",
+      imageUrl: "./images/image-11.jpeg",
       order: 11,
     },
   ]);
