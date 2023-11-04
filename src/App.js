5a42cef7003a8d9d210762f4fc98803ee064636e
nafishasalsabil <nafishasalsabil16@gmail.com>
2023-11-04 21:53:09 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,54 +1,106 @@
-import logo from './logo.svg';
-import React from 'react';
-import Gallery from './components/gallery/gallery.component';
+import React, { useState } from "react";
+import Gallery from "./components/gallery/gallery.component";
 
 const App = () => {
-  const images = [
+  const [dragId, setDragId] = useState();
+  const [images, setImages] = useState([
     {
       id: 1,
-      imageUrl: '/images/image-1.webp',
+      imageUrl: "/images/image-1.webp",
+      order: 1,
     },
     {
       id: 2,
-      imageUrl: '/images/image-2.webp',
-    },{
-      id: 3,
-      imageUrl: '/images/image-3.webp',
-    },{
-      id: 4,
-      imageUrl: '/images/image-4.webp',
-    },{
-      id: 5,
-      imageUrl: '/images/image-5.webp',
-    },{
-      id: 6,
-      imageUrl: '/images/image-6.webp',
-    },{
-      id: 7,
-      imageUrl: '/images/image-7.webp',
-    },{
-      id: 8,
-      imageUrl: '/images/image-8.webp',
-    },{
-      id: 9,
-      imageUrl: '/images/image-9.webp',
-    },{
-      id: 10,
-      imageUrl: '/images/image-10.jpeg',
-    },{
-      id: 11,
-      imageUrl: '/images/image-11.jpeg',
+      imageUrl: "/images/image-2.webp",
+      order: 2,
     },
-  ];
+    {
+      id: 3,
+      imageUrl: "/images/image-3.webp",
+      order: 3,
+    },
+    {
+      id: 4,
+      imageUrl: "/images/image-4.webp",
+      order: 4,
+    },
+    {
+      id: 5,
+      imageUrl: "/images/image-5.webp",
+      order: 5,
+    },
+    {
+      id: 6,
+      imageUrl: "/images/image-6.webp",
+      order: 6,
+    },
+    {
+      id: 7,
+      imageUrl: "/images/image-7.webp",
+      order: 7,
+    },
+    {
+      id: 8,
+      imageUrl: "/images/image-8.webp",
+      order: 8,
+    },
+    {
+      id: 9,
+      imageUrl: "/images/image-9.webp",
+      order: 9,
+    },
+    {
+      id: 10,
+      imageUrl: "/images/image-10.jpeg",
+      order: 10,
+    },
+    {
+      id: 11,
+      imageUrl: "/images/image-11.jpeg",
+      order: 11,
+    },
+  ]);
+  const handleDrag = (ev, id) => {
+    setDragId(id);
+    console.log(dragId);
+    console.log(images);
+  };
+
+  const handleDrop = (ev) => {
+    const dragBox = images.find((box) => box.id === dragId);
+    var idc = ev.currentTarget.id;
+    const dropBox = images.find((box) => String(box.id) === String(idc));
+    if (dragBox && dropBox) {
+      const dragBoxOrder = dragBox.order;
+      const dropBoxOrder = dropBox.order;
+
+      const newBoxState = images.map((box) => {
+        var curV = ev.currentTarget.id;
+        if (box.id === dragId) {
+          box.order = dropBoxOrder;
+          console.log("hi");
+        }
+        if (box.id === Number(curV)) {
+          box.order = dragBoxOrder;
+          console.log(box.order);
+        }
+        return box;
+      });
+
+      setImages(newBoxState);
+      console.log(images);
+    }
+  };
+
   return (
-    <div className='gallery-container'>
-      {/* {images.map((item) => ( // No need to destructure here
-        <Gallery key={item.id} item={item} /> // Pass the entire item object
-      ))} */}
-      <Gallery images={images} />
-      
+    <div className="gallery-container">
+      <Gallery
+        images={images}
+        handleDrag={handleDrag}
+        handleDrop={handleDrop}
+      />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/gallery/gallery.component.jsx
+++ src/components/gallery/gallery.component.jsx
@@ -1,34 +1,32 @@
-import './gallery.styles.scss';
-import React, { useState,useEffect } from 'react';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import "./gallery.styles.scss";
+import React, { useState, useEffect } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-
-
-const Gallery = ({ images }) => {
+const Gallery = ({ images, handleDrag, handleDrop }) => {
   const [selectedImages, setSelectedImages] = useState([]);
-  const [imagesAll, setImages] = useState([]);
+  var [imagesAll, setImages] = useState([]);
+  imagesAll = imagesAll.slice().sort((a, b) => a.order - b.order);
 
   const handleSelectAll = () => {
     if (selectedImages.length > 0) {
-      // If all images are already selected, clear the selection
       setSelectedImages([]);
     } else {
-      // Otherwise, select all images
       setSelectedImages(imagesAll.map((item) => item.id));
     }
   };
 
   const handleDeleteSelected = () => {
-    const remainingImages = imagesAll.filter((item) => !selectedImages.includes(item.id));
-    console.log(remainingImages);
+    const remainingImages = imagesAll.filter(
+      (item) => !selectedImages.includes(item.id)
+    );
     setImages(remainingImages);
+    console.log(imagesAll);
     setSelectedImages([]);
   };
-  
+
   useEffect(() => {
     setImages(images);
-  }, [])
+  }, []);
 
   const handleImageClick = (imageId) => {
     if (selectedImages.includes(imageId)) {
@@ -37,104 +35,72 @@ const Gallery = ({ images }) => {
       setSelectedImages([...selectedImages, imageId]);
     }
   };
-
-  const handleDragEnd = (result) => {
-    console.log(result);
-    if (!result.destination) {
-      return;
-    }
-    // console.log(result.destination);
-    else if(result.destination){
-      const reorderedImages = reorder(
-        imagesAll,
-        result.source.index,
-        result.destination.index
-      );
-      setImages(reorderedImages);
-    }
-  
-   
-  };
-
-  const reorder = (list, startIndex, endIndex) => {
-    const result = Array.from(list);
-    const [removed] = result.splice(startIndex, 1);
-    result.splice(endIndex, 0, removed);
-  
-    return result;
-  };
-  
   return (
-    <DragDropContext onDragEnd={handleDragEnd}>
-      <div className="container-fluid">
-        <div className="row mt-3 mb-4 px-2">
-          <div className="col col-lg-6">
-            <h2 className='title'>Image Gallery</h2>
-            <h5>{selectedImages.length} files selected</h5>
-          </div>
-          <div className="col col-lg-6">
-            <button className='btn btn-outline-dark mx-1' onClick={handleSelectAll}>
-              {selectedImages.length > 0 ? "Clear Selection" : "Select All"}
-            </button>
-            {selectedImages.length > 0 && (
-              <button className='btn btn-outline-danger mx-1' onClick={handleDeleteSelected}>
-                Delete Selected Items
-              </button>
-            )}
-          </div>
+    <div className="container-fluid">
+      <div className="row mt-3 mb-4 px-2">
+        <div className="col col-lg-6">
+          <h2 className="title">Image Gallery</h2>
+          <span className="file-num">
+            {selectedImages.length === 1
+              ? selectedImages.length + " file selected"
+              : selectedImages.length + " files selected"}
+          </span>
+          <span className="clicked-file">(Click on the image to select)</span>
         </div>
-        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200: 4 }}>
-          <Droppable  droppableId="image-gallery" direction="horizontal">
-            {(provided) => (
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-                className="masonry-container"
-              >
-                <Masonry columnsCount={4} gutter="2px" position="center">
-                  {imagesAll.map((item, index) => (
-                    <label className="image-label">
-                <div
-                          className={`masonry-item ${selectedImages.includes(item.id) ? 'selected' : ''}`}
-                        >
-                        
-                          <Draggable key={String(item.id)} draggableId={String(item.id)} index={index}>
-                      {(provided, snapshot) => (
-                        <div
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                    
-                              className='image-container'
-                              key={item.id}
-                            >
-                              <input
-                                type="checkbox"
-                                checked={selectedImages.includes(item.id)}
-                                onChange={() => handleImageClick(item.id)}
-                                style={{ display: 'none' }}
-                              />
-                              <img
-                                src={item.imageUrl}
-                                alt=""
-                                className={`gallery-image ${getColumnNumber(index, 11) === 4 ? 'big-image' : ''}`}
-                              />
-                            </div>
-                            )}
-                    </Draggable>
-                         
-                        </div>
-                        </label>
-                    
-                  ))}
-                </Masonry>
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </ResponsiveMasonry>
+        <div className="col col-lg-6">
+          <button
+            className="btn btn-outline-dark mx-1"
+            onClick={handleSelectAll}
+          >
+            {selectedImages.length > 0 ? "Clear Selection" : "Select All"}
+          </button>
+          {selectedImages.length > 0 && (
+            <button
+              className="btn btn-outline-danger mx-1"
+              onClick={handleDeleteSelected}
+            >
+              Delete Selected Items
+            </button>
+          )}
+        </div>
       </div>
-    </DragDropContext>
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3, 1200: 4 }}
+      >
+        <Masonry columnsCount={4} gutter="2px" position="center">
+          {imagesAll.map((item, index) => (
+            <label className="image-label">
+              <div
+                className={`masonry-item ${
+                  selectedImages.includes(item.id) ? "selected" : ""
+                }`}
+              >
+                <div className="image-container" key={item.id}>
+                  <input
+                    type="checkbox"
+                    checked={selectedImages.includes(item.id)}
+                    onChange={() => handleImageClick(item.id)}
+                    style={{ display: "none" }}
+                  />
+                  <img
+                    src={item.imageUrl}
+                    alt=""
+                    id={item.id}
+                    className={`gallery-image ${
+                      getColumnNumber(index, 11) === 4 ? "big-image" : ""
+                    }`}
+                    draggable={true}
+                    onDragStart={(ev) => handleDrag(ev, item.id)}
+                    onDragOver={(ev) => ev.preventDefault()}
+                    onDrop={(ev) => handleDrop(ev)}
+                  />
+                </div>
+              </div>
+            </label>
+          ))}
+        </Masonry>
+      </ResponsiveMasonry>
+    </div>
   );
 
   function getColumnNumber(index, numColumns) {
