b8689c802b1690b5b78a7fcaf105a28c550a4bdf
nafishasalsabil <nafishasalsabil16@gmail.com>
2023-11-05 19:39:44 +0600

    complete

--- src/components/gallery/gallery.component.jsx
+++ src/components/gallery/gallery.component.jsx
@@ -38,8 +38,8 @@ const Gallery = ({ images, handleDrag, handleDrop }) => {
   };
   return (
     <div>
-      <div class="container-fluid first">
-        <div className="row mt-3 mb-4 px-2">
+      <div class="container first">
+        <div className="row mt-5 mb-4 px-2">
           <div className="col col-lg-6">
             <h2 className="title">Image Gallery</h2>
             <span className="file-num">
@@ -51,23 +51,23 @@ const Gallery = ({ images, handleDrag, handleDrop }) => {
           </div>
           <div className="col col-lg-6">
             <button
-              className="btn btn-outline-dark mx-1"
+              className="btn btn-outline-dark mx-1 mt-2"
               onClick={handleSelectAll}
             >
               {selectedImages.length > 0 ? "Clear Selection" : "Select All"}
             </button>
             {selectedImages.length > 0 && (
               <button
-                className="btn btn-outline-danger mx-1"
+                className="btn btn-outline-danger mx-1 mt-2"
                 onClick={handleDeleteSelected}
               >
-                Delete Selected Items
+                Delete Selection
               </button>
             )}
           </div>
         </div>
       </div>
-      <div class="container-fluid second">
+      <div class="container-xl second">
         {imagesAll.map((item, index) => (
           <div
             className={`item ${
